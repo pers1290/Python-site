c74b72b6b8b63a0cdac8536184d746af8b4e5a33
user <irin-shkuratov8@yandex.ru>
2024-03-19 16:39:01 +0300

    /

--- main.py
+++ main.py
@@ -14,7 +14,24 @@ def tinttye():
     users = cursor.fetchall()
     connection.commit()
     connection.close()
-    return render_template('push.html', file_list=users)
+    len_db = len(users)
+    index_list = []
+    len_half = (len_db // 2) + 1
+    if len_db % 2 == 0:
+        for i in range(0, len_half, 2):
+            index_list.append(i)
+    else:
+        for i in range(0, len_half + 1, 2):
+            index_list.append(i)
+        users.append(('', '', '', '', '', '/static/img_2/MARS-6.png'))
+    print(users)
+    print(index_list)
+    return render_template('push.html', file_list=users, index_list=index_list)
+
+
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    return render_template('registration.html')
 
 
 if __name__ == '__main__':
