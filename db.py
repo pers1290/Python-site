d9a53017b60f653d34c7ad319b72bc501a731e4b
user <irin-shkuratov8@yandex.ru>
2024-03-23 18:07:59 +0300

    /

--- db.py
+++ db.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('db/User.db')
+connection = sqlite3.connect('db/Reg.db')
 cursor = connection.cursor()
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS Reg (
 id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
-topic TEXT NOT NULL,
-post_text TEXT NOT NULL,
-img_url TEXT NOT NULL
+password TEXT NOT NULL,
+phone TEXT NOT NULL,
+favourites TEXT NOT NULL
 )
 ''')
-cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
-               ('Andy', 'комната', '', '/static/img/m1.jpg'))
-cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
-               ('Bill', 'комната', '', '/static/img/m2.jpg'))
-cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
-               ('Mark', 'комната', '', '/static/img/m3.jpg'))
+# cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
+# ('Andy', 'комната', '', '/static/img/m1.jpg'))
+# cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
+# ('Bill', 'комната', '', '/static/img/m2.jpg'))
+# cursor.execute('INSERT INTO Users (name, topic, post_text, img_url) VALUES (?, ?, ?, ?)',
+# ('Mark', 'комната', '', '/static/img/m3.jpg'))
 connection.commit()
 connection.close()
